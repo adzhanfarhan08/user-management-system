e2853fa6661ea780bab1a46f6fb579d5664bd182
adzhanfarhan <adzhanfarhan@gmail.com>
2024-07-17 19:38:07 +0700

    feat: init

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-center items-center text-center h-screen w-screen">
+      <div class="flex justify-center items-center w-1/4 h-1/2 bg-red-500">
+        <!-- Login -->
+        <div class="">
+          <h1 class="text-2xl">Login Form</h1>
+          <div class="my-5">
+            <label for="" class="block">Username</label>
+            <input type="text" class="border w-full" />
+          </div>
+          <div class="my-5">
+            <label for="" class="block">Password</label>
+            <input type="text" class="border w-full" />
+          </div>
+          <div class="flex justify-between items-center">
+            <p>Remember Me</p>
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+      <!-- Register -->
+      <div class="flex justify-center items-center w-1/4 h-1/2 bg-yellow-500">
+        <div class="">
+          <h1 class="text-2xl">Hello</h1>
+          <p>Don't Have Account ?</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
